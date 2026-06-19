364d33a1636ad62ac840c467aef8a7974b83ace4
kishan <ladkishan99@gmail.com>
2026-06-19 17:04:00 +0530

    Managing the AppDataContext

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -3,14 +3,36 @@ using Order_management.Models;
 
 namespace Order_management.Data
 {
-    public class AppDbContext :DbContext
+    public class AppDBContext : DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
         {
-        }
-        public DbSet<Customer> Customers { get; set; }
-        public DbSet<Category> Categories { get; set; }
-        public DbSet<Product> Products { get; set; }
 
+        }
+        public DbSet<Products> Products { get; set; }
+       
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>(entity =>
+            {
+                entity.HasKey(c => c.Id);
+                entity.Property(c => c.isDeleted)
+                      .HasDefaultValue(false);
+
+                entity.HasQueryFilter(c => !c.isDeleted);
+            });
+            modelBuilder.Entity<Products>(entity =>
+            {
+                entity.HasKey(c => c.Id);
+                entity.Property(c => c.IsDeleted)
+                .HasDefaultValue(false);
+
+                entity.HasQueryFilter(c => !c.IsDeleted);
+            });
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
